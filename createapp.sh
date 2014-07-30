a648d754646091b3d4363d7b6c3d3b04e8f46cfb
root <edward>
2014-07-30 11:30:52 -0700

    check in some conveniece script, add gedit buffer file to gitignore

---
+++ createapp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+###  ------------------------------- ###
+###  Simple utility BASH scripts to  ###
+###  restart app from scratch        ###
+###  ------------------------------- ###
+echo on
+activator -v new social-data-collect akka-sample-cluster-scala
+ 
